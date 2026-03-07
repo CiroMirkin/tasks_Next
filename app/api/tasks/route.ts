0110214d3ca38e2da92756ef108a430b9d5b94d9
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-07 03:06:45 -0300

    feat: agregar paginacion a API de tareas

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -2,8 +2,30 @@
 import { prisma } from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-    const tasks = await prisma.task.findMany()
+export async function GET(request: Request) {
+
+    const { searchParams } = new URL(request.url)
+    const take = Number(searchParams.get('take') ?? '10')
+    const skip = Number(searchParams.get('skip') ?? '0')
+
+    if(isNaN(take)) {
+        return NextResponse.json(
+            { message: 'Take debe ser un numero.', },
+            { status: 400, },
+        )
+    }
+
+    if(isNaN(skip)) {
+        return NextResponse.json(
+            { message: 'Skip debe ser un numero.', },
+            { status: 400, },
+        )
+    }
+
+    const tasks = await prisma.task.findMany({
+        take: take,
+        skip: skip,
+    })
     
     return NextResponse.json({
         tasks,
