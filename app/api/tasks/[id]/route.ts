42304a73b0582eef99d0aeebdf93c0f9997beec2
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-07 04:32:06 -0300

    refactor: extraer logica de obtencion de tarea por ID a funcion separada

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -1,25 +1,37 @@
 import { prisma } from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
+import { Task } from "@/app/generated/prisma/client";
 
 interface Segments {
     params: Promise<{ id: string }>
 }
 
-export async function GET(request: Request, { params }: Segments) {
-    const { id } = await params
-
+const getTaskById = async ({ id }: { id: string}): Promise<{ message: string, status: number, task: Task | null }> => {
     const task = await prisma.task.findUnique({
         where: {
             id,
         }
     })
-
     if(!task) {
-        return NextResponse.json(
-            { message: 'Tarea no encontrada.', },
-            { status: 404, },
-        )
+        return { 
+            task: null,
+            message: 'Tarea no encontrada.', 
+            status: 404, 
+        }
     }
+    return { 
+        task,
+        message: 'Tarea encontrada.', 
+        status: 200, 
+    }
+}
 
+export async function GET(request: Request, { params }: Segments) {
+    const { id } = await params
+    const { task, message, status } = await getTaskById({ id })
+    
+    if(!task) {
+        return NextResponse.json({ message, status })
+    }
     return NextResponse.json(task)
 }
