8b53c2ac59f788d13391edc546aa2a1efc9aef05
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-07 21:19:15 -0300

    feat: agregar funcion createTask al API client

--- app/dashboard/api/tasks.ts
+++ app/dashboard/api/tasks.ts
@@ -11,3 +11,15 @@ export const updateTask = async (id: string, complete: boolean): Promise<Task> =
     }).then(res => res.json())
     return task
 }
+
+export const createTask = async (description: string): Promise<Task> => {
+    const body = { description }
+    const task = await fetch(`/api/tasks`, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+    return task
+}
