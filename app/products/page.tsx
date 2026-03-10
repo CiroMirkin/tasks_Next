3184588fd5d0ce295bb77337d6cada19c4baf589
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-10 04:01:08 -0300

    feat: agregar enlace al carrito en pagina de productos

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,13 +3,14 @@
 import { ShoppingCart } from "lucide-react";
 import ProductList from "./components/ProductList";
 import { cookies } from "next/headers";
+import Link from "next/link";
 
 export default async function ProductsPage() {
 
     const cookieStore = await cookies()
     const cart = JSON.parse(cookieStore.get('cart')?.value ?? '{}')
 
-    const getTotalCount = () => {
+    const totalItemsInCart = (() => {
         let items = 0
 
         Object.values(cart).forEach(value => {
@@ -17,13 +18,20 @@ export default async function ProductsPage() {
         })
 
         return items
-    }
+    })()
 
     return (
         <>
-            <header className="w-full px-8 mb-4 flex items-center justify-end gap-2">
-                <span>{ getTotalCount() }</span>
-                <ShoppingCart/>
+            <header className="w-full px-8 mb-4 flex items-center justify-end">
+                {totalItemsInCart > 0 && (
+                    <Link 
+                        href={'products/cart'}
+                        className="p-2 flex items-center justify-end gap-2 bg-green-100 rounded"
+                    >
+                        <span>{ totalItemsInCart }</span>
+                        <ShoppingCart/>
+                    </Link>
+                )}
             </header>
             <ProductList />
         </>
