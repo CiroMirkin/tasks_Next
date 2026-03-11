1145a9011d10c68fb8deefa78b123d6d07ee6753
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-11 09:59:04 -0300

    feat: actualizar AppSidebar para usar nuevos componentes

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,78 +1,27 @@
-"use client"
-
 import * as React from "react"
 
-import { SearchForm } from "@/components/search-form"
 import {
   Sidebar,
   SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
+  SidebarFooter,
   SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
   SidebarRail,
 } from "@/components/ui/sidebar"
-import { Clipboard, Home, Store } from "lucide-react"
-import { usePathname } from "next/navigation"
+import User from "./user"
+import LogoutAndSignInButton from "./LogoutAndSignInButton"
+import SidebarContentList from "./sidebarContent"
 
-const data = {
-  navMain: [
-    {
-      title: "Getting Started",
-      url: "#",
-      items: [
-        {
-          title: "Home",
-          url: "/",
-          icon: <Home />,
-        },
-        {
-          title: "Tasks Admin",
-          url: "/dashboard",
-          icon: <Clipboard />,
-        },
-        {
-          title: "Products",
-          url: "/products",
-          icon: <Store />,
-        },
-      ],
-    },
-  ]
-}
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  const pathname = usePathname()
-
   return (
     <Sidebar {...props}>
       <SidebarHeader className="">
-        <span className="font-medium py-2 px-2">Tasks</span>
-        <SearchForm />
+        <User />
       </SidebarHeader>
-      <SidebarContent>
-        {data.navMain.map((item) => (
-          <SidebarGroup key={item.title}>
-            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {item.items.map((item) => (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton
-                      isActive={pathname === item.url}
-                      render={<a href={item.url} />}
-                    >
-                      {item.icon}
-                      {item.title}
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        ))}
+      <SidebarContent className="justify-between">
+        <SidebarContentList />
+        <SidebarFooter>
+          <LogoutAndSignInButton />
+        </SidebarFooter>
       </SidebarContent>
       <SidebarRail />
     </Sidebar>
