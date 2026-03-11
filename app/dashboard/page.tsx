7e2f41454bad6503181bd0e3751a537d734ef413
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-11 09:59:15 -0300

    feat: agregar protección de rutas al dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,8 +5,16 @@ import { prisma } from "@/app/lib/prisma"
 import { TaskList } from "./components/TaskList"
 import { CreateTask } from "./components/CreateTask"
 import { DeleteCompletedTasks } from "./components/DeleteCompletedTasks"
+import { getServerSession } from "next-auth"
+import { authOptions } from "../api/auth/[...nextauth]/route"
+import { redirect } from "next/navigation"
 
 export default async function Dashboard() {
+
+  const session = await getServerSession(authOptions)
+  if(!session) {
+    redirect('/api/auth/signin')
+  }
 
   const tasks = await prisma.task.findMany({
     orderBy: {
