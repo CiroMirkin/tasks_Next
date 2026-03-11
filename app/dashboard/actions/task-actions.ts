b50f58ce3b8c573a58cdf2709d2761e2261476c3
Ciro Mirkin <ciromirkin@gmail.com>
2026-03-11 17:22:28 -0300

    feat: modificar createTask para asociar tarea con usuario actual

--- app/dashboard/actions/task-actions.ts
+++ app/dashboard/actions/task-actions.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { prisma } from "@/app/lib/prisma"
+import { getUserServerSession } from "@/auth/actions/auth-actions"
 import { revalidatePath } from "next/cache"
 import { ValidationError } from "yup"
 
@@ -22,7 +23,8 @@ export const toggleTask = async (id: string, complete: boolean) => {
 
 export const createTask = async (newTask: { description: string, complete?: boolean }) => {
     try {
-        const task = await prisma.task.create({ data: newTask, })
+        const user = await getUserServerSession()
+        const task = await prisma.task.create({ data: {...newTask, userId: user!.id}, })
         revalidatePath('/dashboard')
         return task
     }
